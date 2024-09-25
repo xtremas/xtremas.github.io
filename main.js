99ea8b16499149086ff9a1cad442b0737565d4bb
xtremas <91406689+xtremas@users.noreply.github.com>
2024-09-25 19:49:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
-
-    menuToggle.addEventListener("click", function() {
-      navMenu.classList.toggle("active");
-    });
-  });
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Load Header
-  fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header-placeholder').innerHTML = data;
-    })
-    .catch(error => console.log('Error loading header:', error));
-
-  // Load Footer
-  fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer-placeholder').innerHTML = data;
-    })
-    .catch(error => console.log('Error loading footer:', error));
-});
+// Load header
 fetch('header.html')
   .then(response => {
-    console.log(response);  // Tambahkan ini untuk melihat respons
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -36,6 +9,17 @@ fetch('header.html')
   .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
   })
-  .catch(error => console.error('Error fetching header:', error));
+  .catch(error => console.error('Error loading header:', error));
 
-</script>
+// Load footer
+fetch('footer.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading footer:', error));
