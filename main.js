dd33d811b509843404659f242a8667507e1b35bf
xtremas <91406689+xtremas@users.noreply.github.com>
2024-09-25 20:31:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -153,3 +153,21 @@ document.getElementById('search-button').addEventListener('click', () => {
 
 // Initialize products on page load
 fetchProducts(currentPage);
+  // Load header and footer
+        function loadHeaderAndFooter() {
+            fetch('header.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('header').innerHTML = data;
+                })
+                .catch(error => console.error('Error loading header:', error));
+
+            fetch('footer.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('footer').innerHTML = data;
+                })
+                .catch(error => console.error('Error loading footer:', error));
+        }
+
+        loadHeaderAndFooter(); // Call the function to load header and footer
