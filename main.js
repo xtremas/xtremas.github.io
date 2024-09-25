de18d5d52d724b843733683a70811a538c12f614
xtremas <91406689+xtremas@users.noreply.github.com>
2024-09-25 20:02:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,11 @@ $(document).ready(function() {
     }
   });
 });
+  document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+
+    menuToggle.addEventListener("click", function() {
+      navMenu.classList.toggle("active");
+    });
+  });
