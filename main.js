8db617971e09a42df8413e59e76b1ca091e684eb
xtremas <91406689+xtremas@users.noreply.github.com>
2024-09-25 19:33:52 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+
+    menuToggle.addEventListener("click", function() {
+      navMenu.classList.toggle("active");
+    });
+  });
+</script>
